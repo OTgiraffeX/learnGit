2483f099e6539a6b9d4af81153e84bc4e25e0205
OTgiraffe <597035146@qq.com>
2018-04-02 16:30:38 +0800

    项目

---
+++ learnGitTest/learnGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learnGitTest
+//
+//  Created by OTgiraffe on 2018/4/2.
+//  Copyright © 2018年 jieshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ learnGitTest/learnGitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  learnGitTest
+//
+//  Created by OTgiraffe on 2018/4/2.
+//  Copyright © 2018年 jieshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
